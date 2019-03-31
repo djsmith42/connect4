2aa1e1c51e2c272a4abdd665567a97e8bf0ff1cc
Dave Smith <dasmithm@amazon.com>
2019-03-31 16:02:49 -0600

    More tests

--- tests/test_wins.py
+++ tests/test_wins.py
@@ -1,4 +1,4 @@
-from ..players import YELLOW as Y, EMPTY as e
+from ..players import YELLOW as Y, RED as R, EMPTY as e
 from ..wins import find_winning_column
 
 def test_win_horizontal_end_of_row():
@@ -33,3 +33,36 @@ def test_win_vertical_stack_from_base():
         [e, e, e, e, e, e, e],
     ]
     assert find_winning_column(board, Y) == 4
+
+def test_win_vertical_stack_from_base():
+    board = [
+        [e, Y, e, e, Y, Y, e],
+        [e, Y, e, e, Y, e, e],
+        [e, e, e, e, Y, e, e],
+        [e, e, e, e, e, e, e],
+        [e, e, e, e, e, e, e],
+        [e, e, e, e, e, e, e],
+    ]
+    assert find_winning_column(board, Y) == 4
+
+def test_win_diagonal_up_right_and_down_left():
+    board = [
+        [R, Y, R, R, Y, Y, R],
+        [e, Y, R, R, Y, e, e],
+        [e, e, Y, e, Y, e, e],
+        [e, e, Y, e, e, e, e],
+        [e, e, e, e, e, e, e],
+        [e, e, e, e, e, e, e],
+    ]
+    assert find_winning_column(board, Y) == 3
+
+def test_win_diagonal_down_right_and_up_left():
+    board = [
+        [R, Y, R, R, Y, Y, R],
+        [e, Y, Y, R, R, e, e],
+        [e, e, R, e, Y, e, e],
+        [e, e, Y, e, Y, e, e],
+        [e, e, e, e, e, e, e],
+        [e, e, e, e, e, e, e],
+    ]
+    assert find_winning_column(board, Y) == 3
